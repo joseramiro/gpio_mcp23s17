7c862d654424832ad49a30308d6d61328345fcd8
joseramiro <joseramironajeraenriquez@gmail.com>
2025-11-13 13:49:45 +0100

    Modified file version number

--- plib_mcp23s17_spi.c
+++ plib_mcp23s17_spi.c
@@ -2,7 +2,7 @@
  * @file plib_mcp23s17_spi.c
  * @brief Bibliothèque SPI pour le module MCP23S17
  * @author Ramiro Najera
- * @version 1.0.8
+ * @version 1.0.9
  * @date 2025-04-23
  * @copyright Copyright (c) 2025
  */
